920b889668564fd6ce2281b4c489acae4c6c5f39
xFerchoVJ <fer_elsword@hotmail.com>
2023-10-17 12:09:18 -0600

    Register users functionality front

--- js/signin.js
+++ js/signin.js
@@ -3,53 +3,48 @@ const inputNombre = document.querySelector('#nombre'); // Campo de nombre
 const inputApellidoP = document.querySelector('#apellidoP'); // Campo de apellido
 const inputApellidoM = document.querySelector('#apellidoM'); // Campo de apellido
 const inputFoto = document.querySelector('#foto');
-const inputFecha = document.querySelector('#fecha');
-const inputEmailRegistro = document.querySelector('#correo-registro'); // Campo de correo para registro
-const inputPasswordRegistro = document.querySelector('#contrasena-registro'); // Campo de contraseña para registro
+const inputFecha = document.querySelector('#fechaNac');
+const inputEmailRegistro = document.querySelector('#correo'); // Campo de correo para registro
+const inputPasswordRegistro = document.querySelector('#password'); // Campo de contraseña para registro
 
-  formRegistro.addEventListener('submit', async (e) => {
-    e.preventDefault();
-    const formData = new FormData();
-    console.log(inputFoto);
-    const nombre = inputNombre.value;
-    const apellidoP = inputApellidoP.value;
-    const apellidoM = inputApellidoM.value;
-    const profile_image = inputFoto.value;
-    const fecha = inputFecha.value;
-    const emailRegistro = inputEmailRegistro.value;
-    const passwordRegistro = inputPasswordRegistro.value;
+formRegistro.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const formData = new FormData();
+  const nombre = inputNombre.value;
+  const apellidoP = inputApellidoP.value;
+  const apellidoM = inputApellidoM.value;
+  const profile_image = inputFoto.files[0];
+  const fecha = inputFecha.value;
+  const emailRegistro = inputEmailRegistro.value;
+  const passwordRegistro = inputPasswordRegistro.value;
 
-    if ([nombre, apellidoP, apellidoM, profile_image, fecha, emailRegistro, passwordRegistro].includes('')) {
-      return alert('Todos los campos son obligatorios');
+  if ([nombre, apellidoP, apellidoM, profile_image, fecha, emailRegistro, passwordRegistro].includes('')) {
+    return alert('Todos los campos son obligatorios');
+  }
+
+  formData.append('nombre', nombre);
+  formData.append('apellidoP', apellidoP); // Corregido el nombre del campo
+  formData.append('apellidoM', apellidoM); // Corregido el nombre del campo
+  formData.append('profile_image', profile_image);
+  formData.append('fecha', fecha);
+  formData.append('email', emailRegistro);
+  formData.append('password', passwordRegistro);
+
+  try {
+    const request = await fetch('http://localhost:3000/api/usuarios/registrar', {
+      method: 'POST',
+      body: formData, // Configura el cuerpo de la solicitud como FormData
+    });
+    const response = await request.json();
+    if (response.msg == 'Registro guardado con exito') {
+      window.location.href = '/login.html';
+    }else{
+      alert(response.msg);
+      location.reload();
     }
-
-    const data = {
-      nombre,
-      apellidoP,
-      apellidoM,
-      fecha,
-      email: emailRegistro,
-      password: passwordRegistro,
-      profile_image,
-    };
-
-    try {
-      const request = await fetch('http://localhost:3000/api/usuarios/registro', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-      });
-
-      const response = await request.json();
-      console.log(response);
-
-      // Puedes redirigir al usuario a la página de inicio de sesión después del registro
-      location.href = 'login.html';
-
-    } catch (error) {
-      console.log(error);
-    }
-  });
+    console.log(response);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
