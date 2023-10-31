3ac9e9d930627760e158061a69f7bd7d75065eca
MaGarcia011 <75704776+MaGarcia011@users.noreply.github.com>
2023-10-31 03:05:03 -0600

    UserFront3

---
+++ js/metasshow.js
@@ -0,0 +1,27 @@
+const transactionTable = document.getElementById('transaction-tableTwo');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (!localStorage.getItem("token")) {
+        location.href = "MetasMostrar.html";
+    }
+    getTransactions();
+    console.log("entre");
+})
+
+async function getTransactions() {
+    const response = await fetch(`http://localhost:3000/api/usuarios/metasshow/${localStorage.getItem('idUser')}`);
+    const data = await response.json();
+    console.log(data.msg);
+    data.msg.forEach(transaction => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${transaction.Nombre}</td>
+            <td>${transaction.FechaMeta}</td>
+            <td>${transaction.Cantidad}</td>
+            <td>${transaction.Categoria}</td>
+            <td>${transaction.Descripcion}</td>
+        `;
+        transactionTable.appendChild(row);
+    });
+}
