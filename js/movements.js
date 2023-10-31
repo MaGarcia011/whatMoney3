a03151774ffb8e77a0059a87e38c3fb3224e4486
= <=>
2023-10-31 01:40:12 -0600

    Balance e historial de movimientos

---
+++ js/movements.js
@@ -0,0 +1,28 @@
+const transactionTable = document.getElementById('transaction-table');
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    if (!localStorage.getItem("token")) {
+        location.href = "HistorialMovimientos.html";
+    }
+    getTransactions();
+    console.log("entre");
+})
+
+
+async function getTransactions() {
+    const response = await fetch(`http://localhost:3000/api/usuarios/movimientos/${localStorage.getItem('idUser')}`);
+    const data = await response.json();
+    console.log(data.msg);
+    data.msg.forEach(transaction => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>${transaction.Fecha}</td>
+            <td>${transaction.Hora}</td>
+            <td>${transaction.Categoria}</td>
+            <td>${transaction.Tipo}</td>
+            <td>${transaction.Monto}</td>
+        `;
+        transactionTable.appendChild(row);
+    });
+}
